0183fbaee162f8a82d34019e19771949ccae4eba
Hammadsoloman <hammadali95@yahoo.com>
2020-09-20 18:57:14 +0300

    add the routes for storeFront app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ToDo-server
 
 
-**Api for Todo App that done in a separate repo using react hooks**
+**Api for Todo App && StoreFront App that done in a separate repo using react hooks for Todo App and Redux for StoreFront**
---
+++ routes/product.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const { Router } = require('express');
+const express = require('express');
+const route = express.Router();
+const productsCrud = require('../lib/models/product.js/product-collection');
+
+
+route.post('/product',postProduct);
+function postProduct(req,res,next){
+    let data = req.body;
+    productsCrud.create(data)
+    .then(productAdded=>{
+        res.json(productAdded);
+    })
+    .catch(next);
+}
+
+// find All categories
+route.get('./product',getAllProducts);
+function getAllProducts(req,res,next){
+    productsCrud.gey()
+    .then(allProducts=>{
+        res.json(allProducts);
+    })
+    .catch(next);
+}
+
+// find category by id (GET)
+
+route.get('/product/:id',getByIdProduct);
+function getByIdProduct(req,res,next){
+    let id = req.params.id;
+    productsCrud.get(id)
+    .then(productId=>{
+        res.json(productId);
+    })
+    .catch(next);
+}
+
+// update specific category 
+route.update('/product/:id',updateProductById);
+function updateProductById(req,res,next){
+    let id = req.params.id;
+    let data = req.bode;
+    productsCrud.update(id,data)
+    .then(updateProduct=>{
+        res.json(updateProduct);
+    })
+    .catch(next);
+}
+
+// delete specific category By Id (DELETE)
+route.delete('/product/:id',deleteProductById);
+function deleteProductById(req,res,next){
+    let id = req.params.id;
+    productsCrud.delete(id)
+    .then(result=>{
+        res.json({delete:`you delete the category has: ${id}`})
+    })
+    .catch(next)
+}
+
+module.exports = route;
